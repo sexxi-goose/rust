fd2e1692a832da5e16967d1267d65094c983bd77
Jennifer Wills <wills.jenniferg@gmail.com>
2021-02-03 12:57:55 -0500

    update tests

--- src/test/ui/closures/2229_closure_analysis/diagnostics/liveness.rs
+++ src/test/ui/closures/2229_closure_analysis/diagnostics/liveness.rs
@@ -1,5 +1,3 @@
-// edition:2018
-// check-pass
 #![feature(capture_disjoint_fields)]
 //~^ WARNING: the feature `capture_disjoint_fields` is incomplete
 #![allow(unreachable_code)]
@@ -14,28 +12,28 @@ pub fn f() {
     let mut c = Point{ x:1, y:0 };
 
     // Captured by value, but variable is dead on entry.
-    move || {
+    (move || {
         c.x = 1;
         println!("{}", c.x);
-    };
+    })();
 
     // Read and written to, but never actually used.
-    move || {
+    (move || {
         c.x += 1;
-    };
+    })();
 
-    move || {
+    (move || {
         println!("{}", c.x);
         // Value is read by closure itself on later invocations.
         c.x += 1;
-    };
+    })();
     let b = Box::new(42);
-    move || {
+    (move || {
         println!("{}", c.x);
         // Never read because this is FnOnce closure.
         c.x += 1;
         drop(b);
-    };
+    })();
 }
 
 #[derive(Debug)]
@@ -47,16 +45,16 @@ struct MyStruct<'a>  {
 pub fn nested() {
     let mut d = MyStruct{ x: None, y: 1};
     let mut e = MyStruct{ x: None, y: 1};
-    || {
-        || {
+    (|| {
+        (|| {
             d.x = Some("d1");
             d.x = Some("d2");
-        };
-        move || {
+        })();
+        (move || {
             e.x = Some("e1");
             e.x = Some("e2");
-        };
-    };
+        })();
+    })();
 }
 
 fn main() {}
